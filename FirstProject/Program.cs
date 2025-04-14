fe428f469a17bd5d322c287d20465dbdc6a872b8
Deniz3546 <denizyazilm@gmail.com>
2025-04-14 15:58:57 +0300

    Initial commit

---
+++ FirstProject/Program.cs
@@ -0,0 +1,16 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        //Console.WriteLine("Merhaba C# Dunyasi");
+        int number = 10;
+        double yaricap = 30.15;
+        bool isOnline = true;
+        string name = "Ahmet";
+        char ilkharf = 'B';
+        Console.WriteLine(ilkharf);
+        Console.WriteLine(name);
+        Console.Read();
+    }
+}
